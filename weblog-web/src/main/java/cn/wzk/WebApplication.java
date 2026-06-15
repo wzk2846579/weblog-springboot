a8c6aae7844d3459e384fb0ec1278719b96926e9
wangzk16 <wangzk16@asiainfo.com>
2026-06-15 18:03:11 +0800

    初始化weblog项目

---
+++ weblog-web/src/main/java/cn/wzk/WebApplication.java
@@ -0,0 +1,17 @@
+package cn.wzk;
+
+// 正确完整导入
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangzk16
+ * @date 2026年06月15日 16:38
+ */
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
---
+++ weblog-web/src/test/java/cn/wzk/WebApplicationTests.java
@@ -0,0 +1,17 @@
+package cn.wzk;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author wangzk16
+ * @date 2026年06月15日 17:59
+ */
+@SpringBootTest
+@Slf4j
+public class WebApplicationTests {
+
+    @Test
+    public void test() {}
+}
